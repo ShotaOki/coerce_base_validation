830533a202e8b966d98abad5d51379afa16713fa
shota oki <oki.08031367557@i.softbank.jp>
2024-10-28 00:17:13 +0900

    update

--- input_coerce.py
+++ input_coerce.py
@@ -66,7 +66,7 @@ def input_coerce(defines: Dict[RequestType, TypeCheck]):
             # 必須チェックを実施する
             required: bool = kwargs.get("required", False)
             # リクエストタイプを取得する
-            request_type: RequestType = kwargs.get("request_type", RequestType.UNKNOWN)
+            request_type: RequestType = kwargs.get("request_type", None)
 
             # デコレートした関数の引数から、検証対象の変数を参照する
             variable = input_parameter.get(key)
@@ -78,63 +78,64 @@ def input_coerce(defines: Dict[RequestType, TypeCheck]):
                     errors[key] = f"Required: {key}"
                 return False
 
-            # リクエストタイプに対応する入力定義を取得する
-            define: TypeCheck = None
-            for d in defines.keys():
-                if request_type.value in d.value:
-                    define = defines[d]
-                    break
+            # 対象のリスト
+            define_list: List[TypeCheck] = defines.values()
 
-            # リクエストタイプが未定義の場合、エラーを返す
-            if define is None:
-                errors[key] = f"Undefined RequestType: {request_type}"
-                return False
+            if request_type is not None:
+                # リクエストタイプに対応する入力定義を取得する
+                # もし引数にリクエストタイプがあるなら、引数のリクエストタイプだけを検証する
+                define: TypeCheck = None
+                for d in defines.keys():
+                    if request_type.value in d.value:
+                        define_list = [defines[d]]
+                        break
 
-            # 型を検証、引数の配列と一致する型なら処理をする
-            target_type = is_equal_type(type(variable), define.input_type)
-            if target_type is not None:
+            for define in define_list:
+                # 型を検証、引数の配列と一致する型なら処理をする
+                target_type = is_equal_type(type(variable), define.input_type)
+                if target_type is not None:
 
-                # 入力型を検証する
-                if target_type == str:
-                    if define.regex is not None:
-                        # 正規表現で検証、一致しないのならエラーを返す
-                        if not re.match(define.regex, variable):
-                            errors[key] = f'Regex Check Error: {key} : "{variable}"'
+                    # 入力型を検証する
+                    if target_type == str:
+                        if define.regex is not None:
+                            # 正規表現で検証、一致しないのならエラーを返す
+                            if not re.match(define.regex, variable):
+                                errors[key] = f'Regex Check Error: {key} : "{variable}"'
+                                return False
+
+                    # coerceが指定されていて、変換後の型と引数の型が異なるなら、キャストする
+                    if define.coerce is not target_type:
+                        # キャスト関数が例外を投げたのなら、エラーを返す
+                        try:
+                            if define.coerce_function is not None:
+                                # キャスト関数が指定されているのなら、それを利用する
+                                coerced = define.coerce_function(variable)
+                            else:
+                                # キャスト関数が指定されていないのなら、型変換を行う
+                                coerced = define.coerce(variable)
+                        except ValueError:
+                            # キャストに失敗したのなら、エラーを返す
+                            errors[key] = f"Cast Error: {key}"
+                            return False
+                    else:
+                        # 変換後の型と引数の型が同じなら、そのままcoerce後の値として扱う
+                        coerced = variable
+
+                    # 事前判定があれば実行する
+                    # 事前判定の例:
+                    #     isfinite -> floatが有限かどうかを判定する
+                    #     not_is_empty -> 空文字、空配列かどうかを判定する
+                    if define.pre_validate is not None:
+                        # 事前判定がFalseを返したのなら、それを実行する
+                        if not define.pre_validate(coerced):
+                            errors[key] = f"Pre-Validate Error: {key}"
                             return False
 
-                # coerceが指定されていて、変換後の型と引数の型が異なるなら、キャストする
-                if define.coerce is not target_type:
-                    # キャスト関数が例外を投げたのなら、エラーを返す
-                    try:
-                        if define.coerce_function is not None:
-                            # キャスト関数が指定されているのなら、それを利用する
-                            coerced = define.coerce_function(variable)
-                        else:
-                            # キャスト関数が指定されていないのなら、型変換を行う
-                            coerced = define.coerce(variable)
-                    except ValueError:
-                        # キャストに失敗したのなら、エラーを返す
-                        errors[key] = f"Cast Error: {key}"
-                        return False
-                else:
-                    # 変換後の型と引数の型が同じなら、そのままcoerce後の値として扱う
-                    coerced = variable
+                    # 装飾した関数の引数に、coerce後の値を設定する
+                    kwargs["coerced"] = coerced
 
-                # 事前判定があれば実行する
-                # 事前判定の例:
-                #     isfinite -> floatが有限かどうかを判定する
-                #     not_is_empty -> 空文字、空配列かどうかを判定する
-                if define.pre_validate is not None:
-                    # 事前判定がFalseを返したのなら、それを実行する
-                    if not define.pre_validate(coerced):
-                        errors[key] = f"Pre-Validate Error: {key}"
-                        return False
-
-                # 装飾した関数の引数に、coerce後の値を設定する
-                kwargs["coerced"] = coerced
-
-                # 装飾した関数を実行する
-                return func(*args, **kwargs)
+                    # 装飾した関数を実行する
+                    return func(*args, **kwargs)
 
             # 検証対象の型と一致しない場合、エラーを返す
             errors[key] = f"Undefined Type: {key}"
